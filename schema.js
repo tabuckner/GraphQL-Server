f88a6eb54efa76a094a1335df40b43f81731c428
Taylor Buckner <tbuckner@theappraisallane.com>
2019-03-19 18:49:11 -0500

    Added json-server integration and some mock data.

--- schema.js
+++ schema.js
@@ -6,24 +6,27 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = require('graphql');
+const axios = require('axios');
 
-// Hardcoded Test Data
-const customers = [
-  { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
-  { id: '2', name: 'Jane Doe', email: 'janed@gmail.com', age: 34 },
-  { id: '3', name: 'Tom Thumb', email: 'tthumb@gmail.com', age: 77 },
-]
+const JSON_SERVER_HOST = 'http://localhost:3000';
+// // Hardcoded Test Data
+// const customers = [
+//   { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
+//   { id: '2', name: 'Jane Doe', email: 'janed@gmail.com', age: 34 },
+//   { id: '3', name: 'Tom Thumb', email: 'tthumb@gmail.com', age: 77 },
+// ]
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
   name: 'Customer',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLInt },
     name: { type: GraphQLString },
     email: { type: GraphQLString },
     age: { type: GraphQLInt }
   })
 });
+
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -31,14 +34,30 @@ const RootQuery = new GraphQLObjectType({
     customer: {
       type: CustomerType,
       args: {
-        id: { type: GraphQLString },
+        id: { type: GraphQLInt },
       }, 
       resolve(parentValue, args) {
-        for(let customer of customers) {
-          if (customer.id === args.id) {
-            return customer;
-          }
-        }
+        // for(let customer of customers) {
+        //   if (customer.id === args.id) {
+        //     return customer;
+        //   }
+        // }
+        
+        // Return JSON data
+        const requestUri = `${JSON_SERVER_HOST}/customers/${args.id}`
+        return axios.get(requestUri)
+          .then((res) => {
+            console.log(res);
+            return res.data
+          }); // TODO: Why res.data?
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        const requestUri = `${JSON_SERVER_HOST}/customers`
+        return axios.get(requestUri)
+          .then(res => res.data);
       }
     }
   }
